059ce6a5b301707998e8a6d9be4d03ef0edfb9e1
Adriel Morales <adriel8152@hotmail.com>
2024-03-03 02:18:21 -0600

    Se agrega funcionalidad para darle animación al modelo

--- main.js
+++ main.js
@@ -13,18 +13,18 @@ document.querySelector("#app").appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 2000);
 
-renderer.setClearColor(0xA3A3A3);
+renderer.setClearColor(0x000000);
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 
-camera.position.set(6, 6, 6);
+camera.position.set(1, 1, 1);
 
 orbit.update();
 
-const grid = new THREE.GridHelper(30, 30);
-scene.add(grid);
+// const grid = new THREE.GridHelper(30, 30);
+// scene.add(grid);
 
 const gtlfLoader = new GLTFLoader();
 const rgbeLoader = new RGBELoader();
@@ -33,6 +33,10 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 4;
 
+let mixer;
+let starfield;
+let hyperspeed;
+
 rgbeLoader.load( './HDR_029_Sky_Cloudy_Env.hdr', ( texture ) => {
   texture.mapping = THREE.EquirectangularReflectionMapping;
   scene.environment = texture;
@@ -40,10 +44,22 @@ rgbeLoader.load( './HDR_029_Sky_Cloudy_Env.hdr', ( texture ) => {
   gtlfLoader.load( './scene.gltf', ( gltf ) => {
     const model = gltf.scene;
     scene.add(model);
+
+    const animationClips = gltf.animations;
+    mixer = new THREE.AnimationMixer(model);
+
+    starfield = mixer.clipAction(animationClips[0]);
+    hyperspeed = mixer.clipAction(animationClips[1]);
+
+    starfield.play();
+
+    animate();
   } );
 } );
 
-function animate( time ) {
+function animate( ) {
+  requestAnimationFrame(animate);
+  mixer.update(0.005); // Actualiza el tiempo de la animación
   renderer.render(scene, camera,);
 }
 
